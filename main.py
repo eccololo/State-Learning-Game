3a2ba89979dbc1f232467282ccc9a7313f111bb6
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-05-18 17:40:37 +0200

    Adding alert message info at begginig of the game that only english alphabet is allowed. Used tkinter to do that.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+from tkinter import *
+from tkinter import messagebox
 
 
 def get_x_y_coordinates_from_map(x, y):
@@ -6,9 +8,12 @@ def get_x_y_coordinates_from_map(x, y):
     print(x, y)
 
 
+root = Tk()
+root.destroy()
 screen = turtle.Screen()
 screen.setup(width=725, height=491)
 screen.title("Guess the States Game by Mateusz Hyla")
+messagebox.showinfo("Special Chars Message", "In this game use only english alphabet.")
 user_choice = screen.textinput(title=f"Country Choice", prompt="Choose: USA / Italy / Poland?").lower()
 countries_shorts = {
     "usa": "usa",
@@ -25,7 +30,7 @@ turtle.shape(image)
 
 game_is_on = True
 while game_is_on:
-    player_answer = screen.textinput(title="Guess the State.", prompt="What's another states name?")
+    player_answer = screen.textinput(title=f"Guess the state of {user_choice}.", prompt="What's another states name?")
     if player_answer:
         print(player_answer)
         break
